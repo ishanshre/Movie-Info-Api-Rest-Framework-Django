0cff43cf948eb034854d2eef14eee17f18be4ce1
ishanshre <ishan.shrestha356@gmail.com>
2022-09-20 14:28:47 +0545

    add field level validation and object level validation

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -17,3 +17,18 @@ class MovieSerializer(serializers.ModelSerializer):
     #     instance.active = validated_data.get('active', instance.active)
     #     instance.save()
     #     return instance
+
+
+    '''Field level validatoin we call a method with validate_<field>(self, value)'''
+    def validate_title(self, value):
+        if len(value)<2:
+            raise serializers.ValidationError("Name is too short")
+        else:
+            return value
+
+    '''Object level validation we call a method to add logic to validate the data'''
+    def validate(self, data):
+        if data['title']==data['descriptions']:
+            raise serializers.ValidationError("Title and description of movie must not be same")
+        else:
+            return data
